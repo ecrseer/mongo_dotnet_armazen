cc87e3b7533f2c8179e4e1d4022ba31a2ff20a06
Gabriel Justino <60169516+ecrseer@users.noreply.github.com>
2020-12-30 16:45:40 -0300

    constructor

--- Controllers/UtilitarioController.cs
+++ Controllers/UtilitarioController.cs
@@ -1,4 +1,6 @@
+using mgApi.Data.Collections;
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
 
 namespace mgApi.Controllers
 {
@@ -6,95 +8,12 @@ namespace mgApi.Controllers
     [Route("[controller]")]
     public class UtilitarioController: ControllerBase
     {
-        private readonly Context _context;
+        Data.MongoDB _mongoDB;
+        IMongoCollection<Utilitario> _utilitarioCollection;
 
-        public UtilitariosController(Context context)
-        {
-            _context = context;
+        public UtilitarioController(Data.MongoDB mgdb){
+
         }
 
-        // GET: api/Utilitarios
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Utilitario>>> GetUtilitarios()
-        {
-            return await _context.Utilitarios.ToListAsync();
-        }
-
-        // GET: api/Utilitarios/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Utilitario>> GetUtilitario(int id)
-        {
-            var utilitario = await _context.Utilitarios.FindAsync(id);
-
-            if (utilitario == null)
-            {
-                return NotFound();
-            }
-
-            return utilitario;
-        }
-
-        // PUT: api/Utilitarios/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutUtilitario(int id, Utilitario utilitario)
-        {
-            if (id != utilitario.id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(utilitario).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!UtilitarioExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/Utilitarios
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<Utilitario>> PostUtilitario(Utilitario utilitario)
-        {
-            _context.Utilitarios.Add(utilitario);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetUtilitario", new { id = utilitario.id }, utilitario);
-        }
-
-        // DELETE: api/Utilitarios/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteUtilitario(int id)
-        {
-            var utilitario = await _context.Utilitarios.FindAsync(id);
-            if (utilitario == null)
-            {
-                return NotFound();
-            }
-
-            _context.Utilitarios.Remove(utilitario);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
-        }
-
-        private bool UtilitarioExists(int id)
-        {
-            return _context.Utilitarios.Any(e => e.id == id);
-        }
     }
 }
