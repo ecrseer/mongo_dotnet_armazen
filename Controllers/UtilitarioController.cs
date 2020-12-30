b97e2cd7e93df488f174d40908508f47da3c7309
Gabriel Justino <60169516+ecrseer@users.noreply.github.com>
2020-12-30 16:41:11 -0300

    dto and controller placeholder

---
+++ Controllers/UtilitarioController.cs
@@ -0,0 +1,100 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace mgApi.Controllers
+{
+    [ApiController]
+    
+    public class UtilitarioController: ControllerBase
+    {
+        private readonly Context _context;
+
+        public UtilitariosController(Context context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Utilitarios
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Utilitario>>> GetUtilitarios()
+        {
+            return await _context.Utilitarios.ToListAsync();
+        }
+
+        // GET: api/Utilitarios/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Utilitario>> GetUtilitario(int id)
+        {
+            var utilitario = await _context.Utilitarios.FindAsync(id);
+
+            if (utilitario == null)
+            {
+                return NotFound();
+            }
+
+            return utilitario;
+        }
+
+        // PUT: api/Utilitarios/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUtilitario(int id, Utilitario utilitario)
+        {
+            if (id != utilitario.id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(utilitario).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UtilitarioExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Utilitarios
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Utilitario>> PostUtilitario(Utilitario utilitario)
+        {
+            _context.Utilitarios.Add(utilitario);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUtilitario", new { id = utilitario.id }, utilitario);
+        }
+
+        // DELETE: api/Utilitarios/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUtilitario(int id)
+        {
+            var utilitario = await _context.Utilitarios.FindAsync(id);
+            if (utilitario == null)
+            {
+                return NotFound();
+            }
+
+            _context.Utilitarios.Remove(utilitario);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool UtilitarioExists(int id)
+        {
+            return _context.Utilitarios.Any(e => e.id == id);
+        }
+    }
+}
--- Models/UtilitarioDto.cs
+++ Models/UtilitarioDto.cs
@@ -1,1 +1,18 @@
 //viewmodel
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Driver.GeoJsonObjectModel;
+
+namespace mgApi.Models
+{
+    public class UtilitarioDto
+    {
+        public DateTime DataNascimento { get; set; }
+        public GeoJson2DGeographicCoordinates Localizacao { get; set; }
+        public string descricao { get; set; }
+        // circular reference-public List<Produto> produtos { get; set; }
+    }
+}
